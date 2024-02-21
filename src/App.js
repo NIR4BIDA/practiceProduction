52bc7184e516f359d94a0bbeeeb93f2319401bb7
Nir Bida <nir42bida@gmail.com>
2024-02-21 12:27:46 +0200

    app changed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,22 @@ import './App.css';
 
 function App() {
   return (
-    <h1>hi niri</h1>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          niriii123
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
