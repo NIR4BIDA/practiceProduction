92e51c31fc12dff6afb5805c4c848adfc53cdf7e
Nir Bida <nir42bida@gmail.com>
2024-02-23 13:54:06 +0200

    change text from branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          niriii123
+          feature changes
         </p>
         <a
           className="App-link"
